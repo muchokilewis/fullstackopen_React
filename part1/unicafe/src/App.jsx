fc25a13221553ebd020a528be3e03f80d4eb407b
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-01-22 16:11:21 +0300

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Statistics = (props) =>{
+console.log(props)
+
+  return (
+    <div>
+            <h1>Statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>Average {props.average}</p>
+      <p>positive {props.positive} %</p>
+    </div>
+
+  )
+
+}
+
 const App = () => {
   //save click of each button to its own state
   const [good, setGood] = useState(0)
@@ -16,14 +34,8 @@ const App = () => {
       <button onClick={() => setGood(good+1)}>good</button>
       <button onClick={() =>setNeutral(neutral+1) }>neutral</button>
       <button onClick={() => setBad(bad+1)}>bad</button>
-
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>Average {average}</p>
-      <p>positive {positive} %</p>
+      
+      <Statistics good={good} neutral={neutral} bad= {bad} average={average} positive={positive}/>
     </div>
   )
 
