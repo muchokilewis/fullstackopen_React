9027a95dde1ea1e98f68d83a30a45e807b48c0c3
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-01-23 11:52:20 +0300

    anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -15,12 +15,17 @@ const App = () => {
     const [selected, setSelected] = useState(0)
     // console.log(anecdotes.length)
     const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+    const [mostVoted, setMostVoted] = useState(0)
 
     const handleVote = () => {
       const newPoints = [...points]
       newPoints[selected] += 1;
       setPoints(newPoints);
       console.log(newPoints)
+
+      if (newPoints[selected] > newPoints[mostVoted]) {
+        setMostVoted(selected)
+      }
      
     }
     console.log(points)
@@ -28,6 +33,10 @@ const App = () => {
       // by multiplying 'Math.random()' by the length of the anecdotes array and flooring the result, you guarantee that the generated index is within the valid range of the array indices (0 to 'anecdotes.length')
       const randomIndex = Math.floor(Math.random() * anecdotes.length)
       setSelected(randomIndex)
+    }
+    
+    const maxVotes = () => {
+
     }
   
     
@@ -41,6 +50,9 @@ const App = () => {
           <p>has {points[selected]} votes</p>
           <button onClick={handleVote}>vote</button>
           <button onClick={nextAnecdote}>next anecdote</button>
+          <h2>Anecdote with the most votes</h2>
+          {anecdotes[mostVoted]}
+          <p>has {points[mostVoted]}</p>
         </div>
     )
 }
