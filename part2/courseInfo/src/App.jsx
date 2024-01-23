2ff3cb368d5c2191656d86e6e006c811410adf6d
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-01-23 17:12:47 +0300

    course info step7 and step8

--- part2/courseInfo/src/App.jsx
+++ part2/courseInfo/src/App.jsx
@@ -15,11 +15,15 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  //calculating total exercises
+  const totalExercises = props.parts.reduce((total, part) => total + part.exercises, 0)
+
   return (
     <div>
       {props.parts.map(part => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
+      <p><b>Total of {totalExercises} exercises </b></p>
     </div>
   );
 }
@@ -52,6 +56,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
