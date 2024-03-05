db3484d806404b398173c220efdfea9b25c0e084
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-03-05 14:54:09 +0300

    The Phonebook step 1

---
+++ part2/The Phonebook/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+  //stores inputs
+  const [persons, setPersons] = useState([{name: 'Lewis', id:uuidv4() }])
+  // handles new inputs(meant for controlling the form input element)
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newName,
+      id: uuidv4 // Generate a unique id for each person
+    }
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+        
+    console.log("button clicked", event.target)
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      {/* <div>debug: {newName}</div> */}
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.id}>
+            {person.name}
+          </li>
+        )}
+      </ul>
+
+    </div>
+  )
+ 
+}
+
+export default App
