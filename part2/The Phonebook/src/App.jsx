450829f6bb4f3ee59e87077b2f0c32685adb369d
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-03-07 22:22:55 +0300

    The phonebook step 6

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
 import Phonebook from "./components/Phonebook";
+
 
 const App = () => {
   //stores inputs
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+  const [persons, setPersons] = useState([])
   // handles new inputs(meant for controlling the form input element)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        console.log('Promise fulfilled')
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
